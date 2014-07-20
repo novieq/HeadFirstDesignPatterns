2623e83504a0892d8fb5f46d7f7670cf622fbbbc
Sayantan <sayghosh@ebay.com>
2014-07-20 16:50:53 -0700

    Initial Project Commit

---
+++ src/main/java/strategypattern/client/Duck.java
@@ -0,0 +1,10 @@
+package strategypattern.client;
+
+/**
+ * Created by sayghosh on 7/20/14.
+ */
+public class Duck {
+    public void swim() {
+        System.out.println("I am the unchanging part of the Duck class. I can swim.");
+    }
+}
